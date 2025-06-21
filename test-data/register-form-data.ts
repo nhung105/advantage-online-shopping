757afd09de4c4f13ea0ca8eedb27a37e2fa0f6aa
NhungBeo <nguyennhung105124@gmail.com>
2025-06-21 10:04:59 +0700

    Refactor RegisterPage class and update RegisterFormData type for improved readability and consistency

--- test-data/register-form-data.ts
+++ test-data/register-form-data.ts
@@ -1,62 +1,62 @@
 export type RegisterFormData = {
-    username: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 
-    firstName?: string;
-    lastName?: string;
-    phoneNumber?: string;
+  firstName?: string;
+  lastName?: string;
+  phoneNumber?: string;
 
-    country?: string;
-    city?: string;
-    address?: string;
-    state?: string
-    postalCode?: string;
+  country?: string;
+  city?: string;
+  address?: string;
+  state?: string;
+  postalCode?: string;
 
-    agreeToTerm: boolean;
+  agreeToTerm: boolean;
 };
 
 const invalidPassword = [
-    {
-        name: "Missing Uppercase",
-        password: "aaa1",
-        expected: "One upper letter required",
-    },
-    {
-        name: "Missing Lowercase",
-        password: "AAA1",
-        expected: "One lower letter required",
-    },
-    {
-        name: "Missing Number",
-        password: "Aaaa",
-        expected: "One number required",
-    },
-    {
-        name: "Only Lowercase Present",
-        password: "aaaa",
-        expected: "One upper letter required",
-    },
-    {
-        name: "Only Uppercase Present",
-        password: "AAAA",
-        expected: "One lower letter required",
-    },
-    {
-        name: "Only Number Present",
-        password: "1234",
-        expected: "One lower letter required",
-    },
-    {
-        name: "input value exceeds length max",
-        password: "dsfsdfsdfsdfs",
-        expected: "Use maximum 12 character",
-    },
-    {
-        name: "input value under length min",
-        password: "dsf",
-        expected: "Use 4 character or longer",
-    },
+  {
+    name: "Missing Uppercase",
+    password: "aaa1",
+    expected: "One upper letter required",
+  },
+  {
+    name: "Missing Lowercase",
+    password: "AAA1",
+    expected: "One lower letter required",
+  },
+  {
+    name: "Missing Number",
+    password: "Aaaa",
+    expected: "One number required",
+  },
+  {
+    name: "Only Lowercase Present",
+    password: "aaaa",
+    expected: "One upper letter required",
+  },
+  {
+    name: "Only Uppercase Present",
+    password: "AAAA",
+    expected: "One lower letter required",
+  },
+  {
+    name: "Only Number Present",
+    password: "1234",
+    expected: "One lower letter required",
+  },
+  {
+    name: "input value exceeds length max",
+    password: "dsfsdfsdfsdfs",
+    expected: "Use maximum 12 character",
+  },
+  {
+    name: "input value under length min",
+    password: "dsf",
+    expected: "Use 4 character or longer",
+  },
 ];
 export default invalidPassword;
