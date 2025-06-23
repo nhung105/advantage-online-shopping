ec6c0d779a4e16a1f40c4133cd5d4eee665782e0
NhungBeo <nguyennhung105124@gmail.com>
2025-06-23 22:48:31 +0700

    Refactor HomePage tests for improved readability and structure; enhance visibility checks for homepage items and hover interactions

--- tests/HomePage.test.ts
+++ tests/HomePage.test.ts
@@ -7,42 +7,74 @@ let homePage: HomePage;
 // const speakersCategory = page.locator('#speakersImg');
 
 test.beforeEach(async ({ page }) => {
-    homePage = new HomePage(page);
-    await homePage.navigateHomePage();
+  homePage = new HomePage(page);
+  await homePage.navigateHomePage();
 });
-test.describe('Verify homepage items display in different screensizes', () => {
-    const screenSizes1 = [{ ...screenSizes[0], ...screenSizes[1] }];
-    screenSizes1.forEach(({ width, height }) => {
-        test(`All homepage items display in ${width} x ${height}`, async ({ page }) => {
-            await homePage.setScreenSizes(width, height);
-            await expect(homePage.logo).toBeVisible();
-            await expect(homePage.ourProductsMenu).toBeVisible();
-            await expect(homePage.specialOfferMenu).toBeVisible();
-            await expect(homePage.popularItemsMenu).toBeVisible();
-            await expect(homePage.contactUsMenu).toBeVisible();
-            await expect(homePage.searchItem).toBeVisible();
-            await expect(homePage.userIcon).toBeVisible();
-            await expect(homePage.shoppingCartIcon).toBeVisible();
-            await expect(homePage.inForIcon).toBeVisible();
-        })
-        test('Homepage items display in 768 x 1024', async ({ page }) => {
-            await homePage.setScreenSizes(768, 1024)
-            await expect(homePage.logo).toBeVisible();
-            await expect(homePage.userIcon).toBeVisible();
-            await expect(homePage.shoppingCartIcon).toBeVisible();
-            await expect(homePage.inForIcon).toBeVisible();
-        })
-        test('Homepage items display in 375 x 812', async ({ page }) => {
-            await homePage.setScreenSizes(375, 812)
-            await expect(homePage.logo).toBeVisible();
-        })
-    })
+test.describe("Verify homepage items display in different screensizes", () => {
+  const screenSizes1 = [{ ...screenSizes[0], ...screenSizes[1] }];
+  screenSizes1.forEach(({ width, height }) => {
+    test(`All homepage items display in ${width} x ${height}`, async ({
+      page,
+    }) => {
+      await homePage.setScreenSizes(width, height);
+      await expect(homePage.logo).toBeVisible();
+      await expect(homePage.ourProductsMenu).toBeVisible();
+      await expect(homePage.specialOfferMenu).toBeVisible();
+      await expect(homePage.popularItemsMenu).toBeVisible();
+      await expect(homePage.contactUsMenu).toBeVisible();
+      await expect(homePage.searchItem).toBeVisible();
+      await expect(homePage.userIcon).toBeVisible();
+      await expect(homePage.shoppingCartIcon).toBeVisible();
+      await expect(homePage.inForIcon).toBeVisible();
+    });
+    test("Homepage items display in 768 x 1024", async ({ page }) => {
+      await homePage.setScreenSizes(768, 1024);
+      await expect(homePage.logo).toBeVisible();
+      await expect(homePage.userIcon).toBeVisible();
+      await expect(homePage.shoppingCartIcon).toBeVisible();
+      await expect(homePage.inForIcon).toBeVisible();
+    });
+    test("Homepage items display in 375 x 812", async ({ page }) => {
+      await homePage.setScreenSizes(375, 812);
+      await expect(homePage.logo).toBeVisible();
+    });
+  });
 });
 
-test.describe('Check hover every category', () => {
-    test('Check hover speakers', async ({ page }) => {
-        await page.locator('#speakersImg').hover();
-        await expect(page.locator('#speakersLink')).toHaveText('Shop Now');
-    })
+test.describe("Display the text Shop Now when hover every category", () => {
+  test("hover speakers", async ({ page }) => {
+    await page.locator("#speakersImg").hover();
+    await expect(page.locator("#speakersLink")).toHaveText("Shop Now");
+  });
+  test("hover tablets", async ({ page }) => {
+    await page.locator("#tabletsTxt").hover();
+    await expect(page.locator("#tabletsLink")).toHaveText("Shop Now");
+  });
+  test("hover headphones", async ({ page }) => {
+    await page.locator("#headphonesTxt").hover();
+    await expect(page.locator("#headphonesLink")).toHaveText("Shop Now");
+  });
+  test("hover laptops", async ({ page }) => {
+    await page.locator("#laptopsTxt").hover();
+    await expect(page.locator("#laptopsLink")).toHaveText("Shop Now");
+  });
+  test("hover mice", async ({ page }) => {
+    await page.locator("#miceTxt").hover();
+    await expect(page.locator("#miceLink")).toHaveText("Shop Now");
+  });
+});
 
-})
+test.describe("display product list when click on a category", () => {
+  test("click on speakers", async ({ page }) => {
+    await page.locator("#speakersImg").click();
+    await expect(page.getByText("BUY NOW")).toBeVisible({ timeout: 10000 });
+  });
+  test("click on tablets", async ({ page }) => {
+    await page.locator("#tabletsImg").click();
+    await expect(page.getByText("BUY NOW")).toBeVisible({ timeout: 10000 });
+  });
+});
+
+test.describe("display product list when click on View details on popular items", () => {
+  test("click on View Details of the first item", async ({ page }) => {});
+});
